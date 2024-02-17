f73fce0e5a81c1582a6f1172bd48bf01d1472cc2
kynan justis <kfjustis@gmail.com>
2024-02-17 11:15:31 -0600

    Add placeholder background and proper graphics filter

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ local gameWidth, gameHeight = 320, 240
 local windowWidth, windowHeight = 640, 480
 
 -- resizable and uses canvas
+love.graphics.setDefaultFilter("nearest", "nearest", 1)
+
 push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {
     fullscreen = false,
     resizable = true,
@@ -12,6 +14,8 @@ push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {
 })
 
 function love.load()
+    bg_slice_1 = love.graphics.newImage("assets/bg_placeholder.png")
+    bg_slice_2 = love.graphics.newImage("assets/bg_placeholder.png")
 end
 
 function love.update(dt)
@@ -19,15 +23,16 @@ end
 
 function love.draw()
     push:start()
-    --black border bars
+    --black border bars and render region
     push:setBorderColor(0, 0, 0, 1)
-    --black background
     love.graphics.clear(0,0,1,1)
 
-    --draw here
+    --draw start
+    love.graphics.draw(bg_slice_1, 0, 0)
+    --draw end
     push:finish()
 end
 
 function love.resize(w, h)
     return push:resize(w, h)
-  end
+end
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Description
-Made for Love Jam 2024!
+Made for Love Jam 2024 with Love 11.5!
 
 # Libraries used
 Push - https://github.com/Ulydev/push (commit: [9c16581](https://github.com/Ulydev/push/commit/9c165816a14c868339c3cd0d22eed65c313c8bf8), MIT License)
